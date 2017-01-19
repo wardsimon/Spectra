0c1fbd6383a375dccb2258bb198b09375187e5ae
Simon Ward <simon.ward@psi.ch>
2017-01-19 16:51:14 +0100

    "Redone @spec1d/combine... Should fix some errors but at the expence of speed"

--- spec1d/@spec1d/combine.m
+++ spec1d/@spec1d/combine.m
@@ -1,6 +1,6 @@
 function s_out = combine(toll,varargin)
 %
-% function s_out = combine(toll,s1,s2,....sn,'method','methods','indexing','relative')
+% function s_out = combine(toll,s1,s2,....sn,'method','methods','indexing','auto')
 %
 % @SPEC1D/COMBINE function to combine two or more spectra.
 %
@@ -15,14 +15,25 @@ function s_out = combine(toll,varargin)
 % Default is 'counts'
 %
 % Depending on the optional 'indexing', points are indexed as
-% 'relative'    : Histogram bining of size 'toll'. This is the same as
-%                 using the rebin function except for multiple spectum.
-%                 The end bin might not be a 'full' bin.
-% 'histogram'   : Histogram bining of size 'toll'. This is the same as
-%                 using the rebin function except for multiple spectum.
-%                 We try to make the first and last element 'toll'/2.
-% 'absolute'	: The gap between the points is  always greater then 'toll'
-%                 before any averaging.
+%                   'auto'   The default 'auto' algorithm chooses a bin
+%                            width to cover the data range and reveal the
+%                            shape of the underlying distribution.
+%                  'scott'   Scott's rule is optimal if X is close to being
+%                            normally distributed, but is also appropriate
+%                            for most other distributions. It uses a bin width
+%                            of 3.5*STD(X(:))*NUMEL(X)^(-1/3).
+%                     'fd'   The Freedman-Diaconis rule is less sensitive to
+%                            outliers in the data, and may be more suitable
+%                            for data with heavy-tailed distributions. It
+%                            uses a bin width of 2*IQR(X(:))*NUMEL(X)^(-1/3),
+%                            where IQR is the interquartile range.
+%                'sturges'   Sturges' rule is a simple rule that is popular
+%                            due to its simplicity. It chooses the number of
+%                            bins to be CEIL(1 + LOG2(NUMEL(X))).
+%                   'sqrt'   The Square Root rule is another simple rule
+%                            widely used in other software packages. It
+%                            chooses the number of bins to be
+%                            CEIL(SQRT(NUMEL(X))).
 % 'legacy'      : Replicate the original @spec1d/combine
 % Default is 'relative' due to speed considerations.
 %
@@ -35,7 +46,7 @@ function s_out = combine(toll,varargin)
 % >r = combine(0.01,s1,s2,s3,'method','weight','indexing',absolute) % combine s1,s2,s3 by
 % weight method and indexing 'absolute'
 
-% Simon Ward 25/01/2016
+% Simon Ward 19/01/2017
 
 
 s_ind = cellfun(@(x) isa(x,'spec1d'),varargin);
@@ -46,7 +57,7 @@ p = inputParser;
 p.CaseSensitive = false;
 p.addRequired('toll',@(x) isnumeric(x) && isreal(x));
 p.addRequired('s_in',@iscell);
-p.addParameter('indexing','relative',@ischar)
+p.addParameter('indexing','auto',@ischar)
 p.addParameter('method','counts',@ischar);
 
 p.parse(toll,s,varargin{:});
@@ -60,46 +71,35 @@ x = vertcat(s(:).x);
 [x, ind ] = sort(x(:));
 y = vertcat(s(:).y); y = y(ind);
 e = vertcat(s(:).e); e = e(ind);
-y_fit = vertcat(s(:).yfit); 
+y_fit = vertcat(s(:).yfit);
 if ~isempty(y_fit)
     y_fit = y_fit(ind);
 end
 
-switch lower(indexing(1))
-    case 'r'
-        % Relative
-        if x(1)==x(2)
-            x(2) = x(2)+eps;
+switch lower(indexing(1:2))
+    case 'au'
+        if isnan(bin)
+            bin = fminsearch(@optim_bin,(max(x)-min(x))/max(ceil(log2(numel(x))+1),1));
         end
-        ind = [1; ceil(cumsum(diff(x(:)))/bin)]; % This is a faster way of [~,~,ind] = histcounts(x(:),min(x):bin:max(x));
-    case 'h'
-        % Histogram
-        ind = 1 + (ceil(x(end)/bin)*bin - floor(x(1)/bin)*bin)/bin - sum(bsxfun(@rdivide,x(:),linspace(floor(x(1)/bin)*bin-bin/2, ceil(x(end)/bin)*bin+bin/2, (ceil(x(end)/bin)*bin - floor(x(1)/bin)*bin)/bin+1))<1,2) +1;
-    case 'a'
-        % Absolute
-        ind = zeros(size(x));
-        ind(1) = 1;
-        xtemp = x(1);
-        for i = 2:length(x)
-            if isempty(xtemp)
-                xtemp = x(i);
-            else
-                xtemp = [xtemp x(i)]; %#ok<AGROW>
-            end
-            if diff(xtemp([1 end])) > bin
-                ind(i) = ind(i-1)+1;
-                xtemp = [];
-            else
-                ind(i) = ind(i-1);
-            end
-        end
-    case 'l'
+        edges = calc_bins(bin);
+    case 'sc'
+        edges = scottsrule(x, min(x), max(x), false);
+    case 'fd'
+        edges = fdrule(x, min(x), max(x), false);
+    case 'st'
+        edges = sturgesrule(x, min(x), max(x), false);
+    case 'sq'
+        edges = sqrtrule(x, min(x), max(x), false);
+    case 'le'
         % legacy
         s_out = combine_legacy(method,bin,s);
         return
     otherwise
         error('spec1d:combine:NotValidIndexing','%s is not a valid indexing method. See documentation',p.Results.method)
 end
+
+ind = arrayfun(@(i) i*((x >= edges(i)) & (x < edges(i+1))),1:(length(edges)-1),'UniformOutput',0);
+ind = sum([ind{:}],2);
 
 % Do a quick check to see if we can use a simple mean (equal monitor measurement)
 m = y./e.^2;
@@ -113,7 +113,8 @@ switch lower(method(1))
         N = accumarray(ind(:),ones(size(x)),[],@sum,NaN);
         xs = accumarray(ind(:),x(:),[],@sum,NaN)./N(:);
         ys = accumarray(ind(:),y(:),[],@sum)./N(:);
-        es = sqrt(accumarray(ind(:),e(:).^2,[],@sum))./N(:);
+        %         es = sqrt(accumarray(ind(:),e(:).^2,[],@sum))./N(:);
+        es = accumarray(ind(:),e(:),[],@norm)./N(:);
         if ~isempty(y_fit)
             y_fit_s = accumarray(ind(:),y_fit(:),[],@sum)./N(:);
         else
@@ -141,6 +142,7 @@ switch lower(method(1))
         end
         % We have attempted to re-establish sqrt statistics
         es = (accumarray(ind(:),(y(:)./m(:)),[],@sum).^0.5)./ms;
+        %         es = accumarray(ind(:),(y(:)./m(:)),[],@norm)./ms;
     case 'w'
         % Weight
         ms = accumarray(ind(:),1./e(:).^2,[],@sum);
@@ -166,4 +168,119 @@ if ~isempty(y_fit_s)
     r.yfit = y_fit_s(~isnan(xs));
 end
 s_out = feval(class(r),r);
+
+
+    function edges = calc_bins(varargin)
+        maxx = max(x);
+        minx = min(x);
+        maximumbins = 62236;
+        xrange = maxx - minx;
+        hardlimits = false;
+        if ~isempty(x) &&  xrange <= 50 && maxx <= flintmax(class(maxx))/2 ...
+                && minx >= -flintmax(class(minx))/2
+            
+            xscale = max(abs(x(:)));
+            if nargin == 0
+                if xrange > maximumbins
+                    % If there'd be more than maximum bins, center them on an appropriate
+                    % power of 10 instead.
+                    binwidth = 10^ceil(log10(xrange/maximumbins));
+                elseif isfloat(x) && eps(xscale) > 1
+                    % If a bin width of 1 is effectively zero relative to the magnitude of
+                    % the endpoints, use a bigger power of 10.
+                    binwidth = 10^ceil(log10(eps(xscale)));
+                else
+                    % Otherwise bins are centered on integers.
+                    binwidth = 1;
+                end
+            else
+                binwidth = varargin{1};
+            end
+            if ~hardlimits
+                minx = binwidth*round(minx/binwidth); % make the edges bin width multiples
+                maxx = binwidth*round(maxx/binwidth);
+                edges = (floor(minx)-.5*binwidth):binwidth:(ceil(maxx)+.5*binwidth);
+            else
+                minxi = binwidth*ceil(minx/binwidth)+0.5;
+                maxxi = binwidth*floor(maxx/binwidth)-0.5;
+                edges = [minx minxi:binwidth:maxxi maxx];
+            end
+        else
+            edges = scottsrule(x,minx,maxx,hardlimits);
+        end
+    end
+
+    function edges = scottsrule(x, minx, maxx, hardlimits)
+        % Scott's normal reference rule
+        if ~isfloat(x)
+            x = double(x);
+        end
+        binwidth = 3.5*std(x)/(numel(x)^(1/3));
+        if ~hardlimits
+            edges = matlab.internal.math.binpicker(minx,maxx,[],binwidth);
+        else
+            edges = matlab.internal.math.binpickerbl(min(x(:)),max(x(:)),minx,maxx,binwidth);
+        end
+    end
+
+    function edges = fdrule(x, minx, maxx, hardlimits)
+        n = numel(x);
+        xrange = max(x(:)) - min(x(:));
+        if n > 1
+            % Guard against too small an IQR.  This may be because there
+            % are some extreme outliers.
+            iq = max(datafuniqr(x),double(xrange)/10);
+            binwidth = 2 * iq * n^(-1/3);
+        else
+            binwidth = 1;
+        end
+        if ~hardlimits
+            edges = matlab.internal.math.binpicker(minx,maxx,[],binwidth);
+        else
+            edges = matlab.internal.math.binpickerbl(min(x(:)),max(x(:)),minx,maxx,binwidth);
+        end
+    end
+
+    function edges = sturgesrule(x, minx, maxx, hardlimits)
+        nbins = max(ceil(log2(numel(x))+1),1);
+        if ~hardlimits
+            binwidth = (maxx-minx)/nbins;
+            if isfinite(binwidth)
+                edges = matlab.internal.math.binpicker(minx,maxx,[],binwidth);
+            else
+                edges = matlab.internal.math.binpicker(minx,maxx,nbins,binwidth);
+            end
+        else
+            edges = linspace(minx,maxx,nbins+1);
+        end
+    end
+
+    function edges = sqrtrule(x, minx, maxx, hardlimits)
+        nbins = max(ceil(sqrt(numel(x))),1);
+        if ~hardlimits
+            binwidth = (maxx-minx)/nbins;
+            if isfinite(binwidth)
+                edges = matlab.internal.math.binpicker(minx,maxx,[],binwidth);
+            else
+                edges = matlab.internal.math.binpicker(minx,maxx,nbins,binwidth);
+            end
+        else
+            edges = linspace(minx,maxx,nbins+1);
+        end
+    end
+
+    function C = optim_bin(b)
+        bb = linspace(b - 0.8*b,b + 0.8*b,10);
+        for i = 1:length(bb)
+            EDG = calc_bins(bb(i));
+            ki = cellfun(@sum,(arrayfun(@(i) (x >= EDG(i)) & (x < EDG(i+1)),1:(length(EDG)-1),'UniformOutput',0)));
+            k = mean(ki);
+            v = sum((ki - k).^2)/(length(EDG) - 1);
+            C(i) = abs((2*k - v)/(bb(i)^2));
+        end
+        C = interp1(bb,C,bb([1 end]),'pchip');
+        C = mean(C);
+    end
+end
+
 
